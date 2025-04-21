ceaf6b65302e253721e738d744ce87037a58dcec
Jose Quintas <juniorquintas@gmail.com>
2025-04-21 02:29:58 +0200

    monorepo changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,9 @@
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   root: './',
   publicDir: './dev/public',
   server: {
     open: './dev/index.html',
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, './src/index.ts'),
-      formats: ['cjs'],
-      fileName: format => `index.${format}.js`,
-    },
-    outDir: 'dist',
-    sourcemap: true,
-    emptyOutDir: true,
-    rollupOptions: {
-      external: [],
-      output: {
-        exports: 'named',
-        globals: {},
-      },
-    },
   },
 });
