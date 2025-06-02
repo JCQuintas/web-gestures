4dc29480f939d6fa29213b56aa27240923077546
Jose Quintas <juniorquintas@gmail.com>
2025-06-02 23:30:54 +0200

    update declarations

--- packages/matchers/src/index.ts
+++ packages/matchers/src/index.ts
@@ -6,10 +6,8 @@ import { ToUpdateState, toUpdateState } from './matchers/toUpdateState';
 export type GestureMatchers<R = unknown> = ToUpdateOptions<R> & ToBeClonable<R> & ToUpdateState<R>;
 
 declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-explicit-any
-  interface Assertion<T = any> extends GestureMatchers<T> {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining extends GestureMatchers {}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
+  interface Matchers<T = any> extends GestureMatchers<T> {}
 }
 
 expect.extend({
