8ce536ff1a4c47b38ad1db57567a7c07cd7daa44
Jose Quintas <juniorquintas@gmail.com>
2025-06-03 00:04:19 +0200

    provide expect extension

---
+++ packages/matchers/expect.d.ts
@@ -0,0 +1,11 @@
+import 'vitest';
+import { ToBeClonable } from './src/matchers/toBeClonable';
+import { ToUpdateOptions } from './src/matchers/toUpdateOptions';
+import { ToUpdateState } from './src/matchers/toUpdateState';
+
+export type GestureMatchers<R = unknown> = ToUpdateOptions<R> & ToBeClonable<R> & ToUpdateState<R>;
+
+declare module 'vitest' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
+  interface Matchers<T = any> extends GestureMatchers<T> {}
+}
