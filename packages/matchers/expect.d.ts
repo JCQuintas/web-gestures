5b40ffc4caf3ae424232e9f572fc9132632e8739
Jose Quintas <juniorquintas@gmail.com>
2025-06-03 00:18:32 +0200

    some types progress

--- packages/matchers/expect.d.ts
+++ packages/matchers/expect.d.ts
@@ -8,4 +8,8 @@ export type GestureMatchers<R = unknown> = ToUpdateOptions<R> & ToBeClonable<R> 
 declare module 'vitest' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
   interface Matchers<T = any> extends GestureMatchers<T> {}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
+  interface Assertion<T = any> extends GestureMatchers<T> {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface AsymmetricMatchersContaining extends GestureMatchers {}
 }
